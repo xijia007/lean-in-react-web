ceec0cad63aa89b87b067eb45449adc04ba31d29
KylieLLL <xu.yuanh@northeastern.edu>
2023-04-17 10:35:40 -0700

    homepage

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../Firebase/firebase';
+import JobsSideBar from "../JobsSideBar/index.jsx";
 
 import { findUsers, findUser } from '../../services/user-service';
 
@@ -61,7 +62,19 @@ function Home() {
   return (
     <nav>
       <h2>Welcome to LeanIn {email}</h2>
+      <div className="row mt-2">
+        <div className="col-2 col-md-2 col-lg-1 col-xl-2">
 
+          <p> 1234</p>
+        </div>
+        <div className="col-9 col-md-10 col-lg-7 col-xl-6"
+             style={{"position": "relative"}}>
+          <p> 1234</p>
+        </div>
+        <div className="col-3 col-lg-2 col-xl-4">
+          <JobsSideBar/>
+        </div>
+      </div>
       <div>
         <button onClick={handleLogout}>Logout</button>
       </div>
